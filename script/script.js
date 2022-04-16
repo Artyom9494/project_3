7e94d5ce7f0522c80d714e3364923b4136b87f67
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2022-04-16 20:02:17 +0300

    create manu

--- script/script.js
+++ script/script.js
@@ -1,11 +1,50 @@
-let button = document.querySelector('.menu__link');
+let button1 = document.querySelector('.activ1');
+let button2 = document.querySelector('.activ2');
+let button3 = document.querySelector('.activ3');
+let button4 = document.querySelector('.activ4');
+let button5 = document.querySelector('.activ5');
 
-button.addEventListener('click', function (e) {
+button1.addEventListener('click', function (e) {
     e.preventDefault();
-    button.classList.add('menu__activ')
+    button1.classList.add('menu__activ')
+    button2.classList.remove('menu__activ')
+    button3.classList.remove('menu__activ')
+    button4.classList.remove('menu__activ')
+    button5.classList.remove('menu__activ')
 })
 
-button.addEventListener('click', function (e) {
+button2.addEventListener('click', function (e) {
     e.preventDefault();
-    button.classList.add('menu__activ')
+    button1.classList.remove('menu__activ')
+    button2.classList.add('menu__activ')
+    button3.classList.remove('menu__activ')
+    button4.classList.remove('menu__activ')
+    button5.classList.remove('menu__activ')
+})
+
+button3.addEventListener('click', function (e) {
+    e.preventDefault();
+    button1.classList.remove('menu__activ')
+    button2.classList.remove('menu__activ')
+    button3.classList.add('menu__activ')
+    button4.classList.remove('menu__activ')
+    button5.classList.remove('menu__activ')
+})
+
+button4.addEventListener('click', function (e) {
+    e.preventDefault();
+    button1.classList.remove('menu__activ')
+    button2.classList.remove('menu__activ')
+    button3.classList.remove('menu__activ')
+    button4.classList.add('menu__activ')
+    button5.classList.remove('menu__activ')
+})
+
+button5.addEventListener('click', function (e) {
+    e.preventDefault();
+    button1.classList.remove('menu__activ')
+    button2.classList.remove('menu__activ')
+    button3.classList.remove('menu__activ')
+    button4.classList.remove('menu__activ')
+    button5.classList.add('menu__activ')
 })
