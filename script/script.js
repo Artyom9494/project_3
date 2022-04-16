081c5c8ce4508d53eb28b4f40282a565fffb5bde
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2022-04-16 17:27:42 +0300

    creati JS button

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let button = document.querySelector('.menu__link');
+
+button.addEventListener('click', function (e) {
+    e.preventDefault();
+    button.classList.add('menu__activ')
+})
+
+button.addEventListener('click', function (e) {
+    e.preventDefault();
+    button.classList.add('menu__activ')
+})
